9b26993b866b1ced33927f78b635c344322a06ac
Arundeepmahajan <58620121+Arundeepmahajan@users.noreply.github.com>
2021-03-21 13:28:07 +0530

    Yeh kehte hai kaam
    pehla nasha pehla khumar

---
+++ StartingJava.java
@@ -0,0 +1,8 @@
+/*Java was created wayback in 1995*/
+/*This will be your first program in java*/
+class StartingJava/*This is the starting point basically it is the class that starts java*/{
+	public static void main(String[] args) {//this is the driver or main function if you have already learnt c++ or c you may know this thing
+		System.out.println("Starting Java with Arundeep Mahajan");/*this is the printing statement, more are their will cover further*/
+
+	}
+}
