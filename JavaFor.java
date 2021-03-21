5c7711d4346ae6e4ffc91bd83a15822a2330fb61
Arundeepmahajan <58620121+Arundeepmahajan@users.noreply.github.com>
2021-03-21 14:16:10 +0530

    JavaFor

---
+++ JavaFor.java
@@ -0,0 +1,13 @@
+public class JavaFor {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello I am also learning with you.");
+
+        }
+        String[] cars = { "Volvo", "Maruti Suzuki", "Ford", "Tata", "Morris Garages" };
+        for (String i : cars) {
+            System.out.println(i);
+        }
+    }
+
+}
